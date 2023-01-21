b0480a0cdc55523009b97ca0a562e9fa55f263be
Saymon Damásio <saymon.damasio95@gmail.com>
2023-01-21 16:10:57 -0300

    feat: isolate routes and prisma instance

---
+++ src/routes.ts
@@ -0,0 +1,16 @@
+import { FastifyInstance } from "fastify";
+import { prisma } from "./lib/prisma";
+
+export async function appRoutes(app: FastifyInstance) {
+  app.get("/habits", async () => {
+    const habits = await prisma.habit.findMany({
+      where: {
+        title: {
+          startsWith: "Beb",
+        },
+      },
+    });
+
+    return habits;
+  });
+}
--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,12 @@
 import Fastify from "fastify";
-import { PrismaClient } from "@prisma/client";
 import cors from "@fastify/cors";
+import { prisma } from "./lib/prisma";
+import { appRoutes } from "./routes";
 
 const app = Fastify();
-const prisma = new PrismaClient();
 
 app.register(cors);
-
-app.get("/habits", async () => {
-  const habits = await prisma.habit.findMany({
-    where: {
-      title: {
-        startsWith: "Beb",
-      },
-    },
-  });
-
-  return habits;
-});
+app.register(appRoutes);
 
 app
   .listen({
