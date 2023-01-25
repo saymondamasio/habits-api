6233da236a1534c0741afed7244a10b9706bfe24
Saymon Damásio <saymon.damasio95@gmail.com>
2023-01-25 20:28:57 -0300

    fix: return completed habits

--- src/routes.ts
+++ src/routes.ts
@@ -68,7 +68,8 @@ export async function appRoutes(app: FastifyInstance) {
 
     console.log(day);
 
-    const completedHabits = day?.dayHabits.map((dayHabit) => dayHabit.habit_id);
+    const completedHabits =
+      day?.dayHabits.map((dayHabit) => dayHabit.habit_id) ?? [];
 
     return {
       possibleHabits,
